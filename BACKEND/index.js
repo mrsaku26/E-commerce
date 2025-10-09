72e96d86103319a04511c78fda22ed8f178a49a7
Demo <sangitkc37@gmail.com>
2025-10-09 12:14:39 +0545

    ad

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -284,6 +284,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running locally on port ${port}`);
+  });
+} else {
+  module.exports = app;
+}

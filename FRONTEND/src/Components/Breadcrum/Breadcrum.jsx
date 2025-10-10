9596a62c9fab7af4a723ff8b1f5d7920481d4664
Demo <sangitkc37@gmail.com>
2025-10-10 13:03:10 +0545

    done

--- FRONTEND/src/Components/Breadcrum/Breadcrum.jsx
+++ FRONTEND/src/Components/Breadcrum/Breadcrum.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Breadcrum.css'
+import '../../Components/Styles/Breadcrum.css'
 import arrow_icon from '../Assests/breadcrum_arrow.png'
 
 const Breadcrum = (props) => {
